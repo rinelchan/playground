86fa31989e36b0a0cb9f57ee3d1c41d3b8421f50
Nutwara Boonyangkongkaew <nutwara.nice@mail.kmutt.ac.th>
2022-03-07 11:44:46 +0700

    first commit

---
+++ calculator/src/App-calculator.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, computed } from 'vue';
+const num1 = ref();
+const num2 = ref();
+const sum = ref();
+
+// const calculate = () => {
+//   sum.value = num1.value + num2.value;
+// };
+
+const calculate = computed(() => 
+  num1.value + num2.value
+);
+
+</script>
+ 
+<template>
+  <div>
+    <input type="number" v-model="num1" /> <br>
+    <input type="number" v-model="num2" /> <br>
+    <!-- <button @click="calculate">calculate</button>
+    <span v-show="sum !== 0">{{ sum }}</span> -->
+    <span>{{ calculate }}</span>
+  </div>
+</template>
+ 
+<style>
+
+</style>
---
+++ calculator/src/App-test-loop.vue
@@ -0,0 +1,17 @@
+<script setup>
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+</script>
+ 
+<template>
+  <div>
+    <ul>
+      <li v-for ="(number, index) in numbers" :key="index">
+        {{ number }}
+      </li>
+    </ul>
+  </div>
+</template>
+ 
+<style>
+
+</style>
